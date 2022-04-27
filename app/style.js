df12def882128ae89350b7de733e1a36f0e436d2
Haki-Malai <p17mala@ionio.gr>
2022-04-27 15:08:00 +0300

    Some style

--- app/style.js
+++ app/style.js
@@ -2,42 +2,57 @@ import { StyleSheet, Dimensions } from 'react-native';
 import { useFonts } from 'expo-font';
 
 let ScreenHeight = Dimensions.get('window').height;
+let ScreenWidth = Dimensions.get('window').width;
+if (ScreenHeight < ScreenWidth) {
+    ScreenWidth = ScreenWidth/2
+}
 
 export default StyleSheet.create({
   container: {
+    fontFamily: 'Roboto',
     flex: 1,
     height: ScreenHeight,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF'
   },
-  header: {
-    padding: 20
-  },
+  
   welcomeTitle: {
-    fontFamily: 'Roboto',
     fontWeight: 'bold',
     fontSize: '2.5rem',
     marginVertical: '1em',
     textAlign: 'center'
   },
+
   text: {
     lineHeight: '1.5em',
     fontSize: '1.125rem',
-    marginVertical: '1em',
     textAlign: 'center'
   },
+
   link: {
     color: '#1B95E0'
   },
-  code: {
-    fontFamily: 'monospace, monospace'
-  },
+
   welcomeLogo: {
       minWidth: '12em',
       minHeight: '12em',
-      zIndex: 5,
-      display: 'block',
-      opacity: 1
-  }
+  },
+
+  menuPressable: {
+      color: 'black',
+      backgroundColor: '#fc5a03',
+      minWidth: ScreenWidth,
+      borderRadius: '1em',
+      marginVertical: '1em'
+  },
+  textPressable: {
+    lineHeight: '1.5em',
+    fontSize: '1.5rem',
+    fontStyle: 'bold',
+    textAlign: 'center',
+    marginVertical: '1em'
+  },
+
+
 });
